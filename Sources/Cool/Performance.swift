a0423398d3eb8888223b2cd2ad4e29ec63a873f3
Skip R <tinyslices@gmail.com>
2025-05-31 17:51:34 -0700

    update performance formatting

--- Sources/Cool/Performance.swift
+++ Sources/Cool/Performance.swift
@@ -1,5 +1,23 @@
 import Darwin
 
+@inlinable
+func format(nanoseconds nanos: UInt64) -> String {
+  let us = 1000, ms = us * 1000, s = ms * 1000
+
+  guard nanos >= us else { return "\(nanos)ns" }
+  guard nanos >= ms else { return "\(nanos / numericCast(us))µs" }
+  guard nanos >= s else { return "\(nanos / numericCast(ms))ms" }
+
+  return "\((Double(nanos) / Double(s)).rounded())s"
+}
+
+extension StringProtocol {
+  @usableFromInline
+  func displayingForMeasurement() -> some StringProtocol {
+    truncating(toOverallMaxLength: 32).padded(toMinLength: 32)
+  }
+}
+
 @available(macOS 10.15, iOS 13, *)
 @inlinable
 @inline(__always)
@@ -7,7 +25,7 @@ public func measure<T>(_ label: String, @_inheritActorContext _ action: sending 
   let start = clock_gettime_nsec_np(CLOCK_MONOTONIC)
   let result = try await action()
   let end = clock_gettime_nsec_np(CLOCK_MONOTONIC)
-  print("[measure] \(label) took \((end - start) / 1000)µs")
+  print("⏰ \(label.displayingForMeasurement()): \(format(nanoseconds: end - start))")
   return result
 }
 
@@ -18,6 +36,6 @@ public func measure<T>(_ label: String, _ action: () throws -> T) rethrows -> T 
   let start = clock_gettime_nsec_np(CLOCK_MONOTONIC)
   let result = try action()
   let end = clock_gettime_nsec_np(CLOCK_MONOTONIC)
-  print("[measure] \(label) took \((end - start) / 1000)µs")
+  print("⏰ \(label.displayingForMeasurement()): \(format(nanoseconds: end - start))")
   return result
 }
